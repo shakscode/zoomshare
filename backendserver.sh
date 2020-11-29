32ecb533b54d84f193287c8fa9488937638dc4cc
shakscode <53089845+shakscode@users.noreply.github.com>
2020-11-29 20:29:30 +0530

    Update backendserver.sh

--- backendserver.sh
+++ backendserver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 yum install -y httpd
-echo "hey, I am $(hostname -f)" > /var/www/html/index.html
+mkdir -p /var/www/tritium/proton.html
+chmod -R 755 /var/www/
+echo "hey, I am $(hostname -f) tritum" > /var/www/tritium/proton.html
 systemctl start httpd
 systemctl enable httpd
